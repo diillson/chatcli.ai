208a3726e99ab3ff63c4933b9430a5150024213a
Edilson Freitas <diillson@hotmail.com>
2026-02-08 23:27:16 -0300

    Remove legacy blog and related configuration files.

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://chatcli.edilsonfreitas.com',
-  output: 'static',
-  build: {
-    assets: 'assets'
-  }
-});
